58f6c7346db1e24292e2df3266d89b82d01942e8
jonaslod <none>
2023-02-14 18:00:44 +0100

    Adds reusable authenticated fetch funtion

---
+++ src/js/api/auth/authenticatedFetch.mjs
@@ -0,0 +1,28 @@
+import authHeaders from "./authenticatedHeaders.mjs";
+import { baseUrl } from "../constants.mjs";
+
+/**
+ * Sends an authenticated fetch request to the auction API
+ * @param {string} url Which part of the API the fetch request is targeting
+ * @param {*} options Additional data to accompany the fetch request
+ * @returns {*} response.json() || undefined
+ * @example
+ * ```js
+ * const loggedInUser = await authenticatedFetch(`/profiles/${username}`);
+ * ```
+ */
+export default async function authenticatedFetch(url, options = {}) {
+  try {
+    const response = await fetch(baseUrl + url, {
+      headers: authHeaders(),
+      ...options,
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      return undefined;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
